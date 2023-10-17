bc664720b22e69a33f105b11956e033a0e4597bb
RashidMinhaj139 <106482352+RashidMinhaj139@users.noreply.github.com>
2023-10-17 13:37:08 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,75 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import New from './New'
+import New from "./New";
 function App() {
-  // const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=AAPL&apikey=YOUR_API_KEY&page"
-  //       );
-  //       const jsonData = await response.json();
-  //       setData(jsonData.bestMatches);
-  //     } catch (error) {
-  //       console.error("Error fetching data: ", error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
-  // const Navbar = () => {
-  //   return (
-  //     <div className="my_navbar">
-  //       <input className="inpbtnn" placeholder="Enter... " />
-  //       <button className="btnn" >Support</button>
-  //       <button className="btnn" >Api key</button>
-  //     </div>
-  //   );
-  // };
-
-  
-
   return (
-    <><New/></>
-    // <div className="App">
-    //   <h1>API Data  is Cards</h1>
-    //   <Navbar />
-
-    //   <div
-    //     style={{
-    //       textAlign: "center",
-    //       display: "flex",
-    //       flexWrap: "wrap",
-    //       flexDirection: "row",
-    //     }}
-    //   >
-    //     {data?.map((item) => (
-    //       <div
-    //         key={item["1. symbol"]}
-    //         style={{
-    //           alignItems: "center",
-    //           textAlign: "center",
-    //           border: "1px solid #ccc",
-    //           borderRadius: "5px",
-    //           backgroundColor: "pink",
-    //           display: "flex",
-    //           flexDirection: "column",
-    //           padding: "10px",
-    //           margin: "10px",
-    //           width: "200px",
-    //         }}
-    //       >
-    //         <h3>{item["2. name"]}</h3>
-    //         <p>Equity :{item["3. type"]}</p>
-    //         <p>region  :{item["4. region"]}</p>
-    //         <p>marketOpen  :{item["5. marketOpen"]}</p>
-    //         <p>marketClose  : {item["6. marketClose"]}</p>
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
+    <>
+      <New />
+    </>
   );
 }
 
--- src/New.js
+++ src/New.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [data, setData] = useState([]);
-  const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState("");
   const [page, setPage] = useState(1);
 
   useEffect(() => {
@@ -11,10 +11,15 @@ const App = () => {
         const response = await fetch(
           `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchKeyword}&apikey=YOUR_API_KEY&page=${page}`
         );
+        if (!response.ok) {
+            console.log("data fetch nh ho rha");
+
+          throw new Error("Failed to fetch data");
+        }
         const jsonData = await response.json();
         setData(jsonData.bestMatches || []);
       } catch (error) {
-        console.error('Error fetching data: ', error);
+        console.error("Error fetching data: ", error);
       }
     };
     fetchData();
@@ -29,20 +34,22 @@ const App = () => {
         value={searchKeyword}
         onChange={(e) => setSearchKeyword(e.target.value)}
       />
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
         {data &&
           data.map((item) => (
-            <div key={item['1. symbol']} style={cardStyle}>
-              <h3>{item['1. symbol']}</h3>
-              <p>Name: {item['2. name']}</p>
-              <p>Market Open: {item['5. marketOpen']}</p>
-              <p>Market Close: {item['6. marketClose']}</p>
-              <p>Match Score: {item['9. matchScore']}</p>
+            <div key={item["1. symbol"]} style={cardStyle}>
+              <h3>{item["1. symbol"]}</h3>
+              <p>Name: {item["2. name"]}</p>
+              <p>Market Open: {item["5. marketOpen"]}</p>
+              <p>Market Close: {item["6. marketClose"]}</p>
+              <p>Match Score: {item["9. matchScore"]}</p>
             </div>
           ))}
       </div>
       <div>
-        <button onClick={() => setPage((prev) => Math.max(prev - 1, 1))}>Previous Page</button>
+        <button onClick={() => setPage((prev) => Math.max(prev - 1, 1))}>
+          Previous Page
+        </button>
         <button onClick={() => setPage((prev) => prev + 1)}>Next Page</button>
       </div>
     </div>
@@ -50,12 +57,12 @@ const App = () => {
 };
 
 const cardStyle = {
-  border: '1px solid #ccc',
-  borderRadius: '5px',
-  backgroundColor: 'lightpink',
-  padding: '10px',
-  margin: '10px',
-  width: '200px',
+  border: "1px solid #ccc",
+  borderRadius: "5px",
+  backgroundColor: "lightpink",
+  padding: "10px",
+  margin: "10px",
+  width: "200px",
 };
 
 export default App;
